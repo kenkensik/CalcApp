69d18d4d4c29300f916bec120f8f8bcbf15a8519
kenkensik <kenkensik@yahoo.co.jp>
2019-10-11 22:51:06 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kento/saka/calcapp/SecondActivity.kt
@@ -0,0 +1,19 @@
+package jp.techacademy.kento.saka.calcapp
+
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val value1 = intent.getDoubleExtra("VALUE1", 0.0)
+
+        textView.text = "$value1"
+    }
+}
